c5692976eec1c1e0c6f3aeb7817bf45d14ca1fc7
Yannick Voyer <yan.voyer@gmail.com>
2025-04-13 11:23:11 -0400

    * Fix phpcs issue

--- src/Ports/Symfony/SymfonyPublisher.php
+++ src/Ports/Symfony/SymfonyPublisher.php
@@ -38,7 +38,9 @@ final class SymfonyPublisher implements EventPublisher
 
     /**
      * @param ComponentDispatcher|ContractDispatcher $dispatcher
-     * @deprecated Signature will be changed to use "Symfony\Component\EventDispatcher\EventDispatcherInterface" in 3.0. Consider changing your implementations.
+     * @deprecated Signature will be changed to use "Symfony\Component\EventDispatcher\EventDispatcherInterface"
+     * in 3.0. Consider changing your implementations.
+     *
      * @see https://github.com/yvoyer/domain-event/issues/47
      */
     public function __construct(/* todo Uncomment in 3.0 EventDispatcherInterface */$dispatcher)
