9b2a145a1adc74aa67e4e541316bbdd0b67ffcfd
Yannick Voyer <yvoyer@users.noreply.github.com>
2025-06-18 21:04:26 -0400

    Fix #50 - Remove support to Base Query classes (#72)

--- src/Messaging/Query.php
+++ src/Messaging/Query.php
@@ -13,13 +13,5 @@ interface Query extends Message
     /**
      * @param mixed $result
      */
-    public function __invoke($result): void;
-
-    /**
-     * @return mixed
-     * @deprecated Will be removed from the interface in 3.0.
-     * No need to change anything unless you type hinted to Query and used getResult().
-     * @see https://github.com/yvoyer/domain-event/issues/50
-     */
-    public function getResult();
+    public function __invoke(mixed $result): void;
 }
